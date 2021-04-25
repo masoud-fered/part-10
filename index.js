abd6b6c90add8dc091c3d72be7b5cba1cc75db26
Masoud <masoud.fered@gmail.com>
2021-04-25 19:46:33 +0430

    set toFixed(2) for result to prevent result with many decimal

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const btc = new CryptoCurrency('BTC', 1200);
 btc.on(btc.CHANGE_PRICE_EVENT, (currentValue, newValue) => {
     const difference = newValue - currentValue;
     const percentageDiff = difference / currentValue * 100;
-    console.log(percentageDiff + '%')
+    console.log(percentageDiff.toFixed(2) + '%')
 })
 
 
