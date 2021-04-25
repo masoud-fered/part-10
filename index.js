2cb1f201c901b5594c1a3818ebeb0abb402be5eb
Masoud <masoud.fered@gmail.com>
2021-04-25 19:44:44 +0430

    calculate increase and decrease value by percentage on changeValue event

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const {CryptoCurrency} = require('./crypto-currency')
 
 const btc = new CryptoCurrency('BTC', 1200);
 
-btc.on(btc.CHANGE_PRICE_EVENT, value => {
-    console.log(value);
+btc.on(btc.CHANGE_PRICE_EVENT, (currentValue, newValue) => {
+    const difference = newValue - currentValue;
+    const percentageDiff = difference / currentValue * 100;
+    console.log(percentageDiff + '%')
 })
 
 
 btc.price = 1180;
+
+btc.price = 1340;
