cb3eac301fdfc60c1c6cde159c4187ab58abbbd1
Masoud <masoud.fered@gmail.com>
2021-04-25 19:20:22 +0430

    create observe of changePrice event for test

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const {CryptoCurrency} = require('./crypto-currency')
 
 const btc = new CryptoCurrency('BTC', 1200);
+
+btc.on(btc.CHANGE_PRICE_EVENT, value => {
+    console.log(value);
+})
+
+
+btc.price = 1180;
